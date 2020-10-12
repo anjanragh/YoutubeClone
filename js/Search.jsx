b0968829e252e00d34ce463a5f23f1f85e9e8727
anjanragh <anjanragh@gmail.com>
2020-10-11 23:19:48 -0400

    Added search capability

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,15 +1,47 @@
-import React from "react";
+import React, { Component } from "react";
 import preload from "../data.json";
 import ShowCard from "./ShowCard";
 
-const Search = () => (
-  <div className="search">
-    <div>
-      {preload.shows.map((show) => (
-        <ShowCard {...show} key={show.imdbID} />
-      ))}
-    </div>
-  </div>
-);
+class Search extends Component {
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      searchTerm: "",
+    };
+
+    // this.handleSearchTermChange = this.handleSearchTermChange.bind(this);
+  }
+
+  handleSearchTermChange = (event) => {
+    this.setState({ searchTerm: event.target.value });
+  };
+  render() {
+    return (
+      <div className="search">
+        <header>
+          <h1>OurTube</h1>
+          <input
+            onChange={this.handleSearchTermChange}
+            value={this.state.searchTerm}
+            type="text"
+            placeholder="search"
+          />
+        </header>
+        <div>
+          {preload.shows
+            .filter(
+              (show) =>
+                `${show.title} ${show.description}`
+                  .toUpperCase()
+                  .indexOf(this.state.searchTerm.toUpperCase()) >= 0
+            )
+            .map((show) => (
+              <ShowCard {...show} key={show.imdbID} />
+            ))}
+        </div>
+      </div>
+    );
+  }
+}
 export default Search;
