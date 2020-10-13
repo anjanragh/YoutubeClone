fa83b456784adb2724bc7ab096ed4a82fd3bbe4f
anjanragh <anjanragh@gmail.com>
2020-10-13 02:00:41 -0400

    Added click for showcard to link to details page

--- js/ShowCard.jsx
+++ js/ShowCard.jsx
@@ -2,14 +2,17 @@
 
 import React from "react";
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
-const Wrapper = styled.div`
+const Wrapper = styled(Link)`
   width: 32%;
   border: 2px solid #333;
   border-radius: 4px;
   margin-bottom: 25px;
   padding-right: 10px;
   overflow: hidden;
+  color: black;
+  text-decoration: none;
 `;
 
 const Image = styled.img`
@@ -17,13 +20,8 @@ const Image = styled.img`
   float: left;
   margin-right: 10px;
 `;
-const ShowCard = (props: {
-    poster: string,
-    title: string,
-    year: string,
-    description: string
-  }) => (
-  <Wrapper>
+const ShowCard = (props: Show) => (
+  <Wrapper to={`/details/${props.imdbID}`}>
     <Image
       alt={`${props.title} show poster`}
       src={`/public/img/posters/${props.poster}`}
@@ -35,6 +33,5 @@ const ShowCard = (props: {
     </div>
   </Wrapper>
 );
-
 
 export default ShowCard;
